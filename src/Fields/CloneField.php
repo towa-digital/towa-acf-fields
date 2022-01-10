d9cdf7d4531cd84ad7e6f941522ef493e7909e8c
Martin Welte <martin.welte@towa.at>
2022-01-10 14:21:03 +0100

    refactor: use correct intendation

--- src/Fields/CloneField.php
+++ src/Fields/CloneField.php
@@ -4,16 +4,22 @@ namespace Towa\Acf\Fields;
 
 use Towa\Acf\BaseField;
 
-class CloneField extends BaseField {
-  protected $prefix;
-  protected $type = 'clone';
-  protected $name = 'clone';
-  protected $label = 'Clone';
+class CloneField extends BaseField
+{
+    protected $prefix;
+    protected $type = 'clone';
+    protected $name = 'clone';
+    protected $label = 'Clone';
 
-  public function build(array $parameter = [])
-  {
-    $default = ['clone' => []];
+    /**
+     * @param array  $parameter
+     *
+     * @return array
+     */
+    public function build(array $parameter = [])
+    {
+        $default = ['clone' => []];
 
-    return parent::build(array_merge($default, $parameter));
-  }
+        return parent::build(array_merge($default, $parameter));
+    }
 }
